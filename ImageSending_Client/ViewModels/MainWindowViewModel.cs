b8a0b15c76099be735e71e2cbd3764d3f834b64d
Ahme_if16 <Ahme_if16@STHQ012B-08>
2023-01-14 17:38:13 +0400

    Add project files.

---
+++ ImageSending_Client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageSending_Client.ViewModels
+{
+    public class MainWindowViewModel
+    {
+    }
+}
